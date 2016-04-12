5d5d9928880bc3789cdf230b5f4d976fbce36d40
Aaron Del Bosque <adelbosque6088@rm-mba-cte-30.attlocal.net>
2016-04-12 10:07:00 -0500

    Commit 1

---
+++ php/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+	<title>Clan War Network</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Links Pure 0.6.0 CSS -->
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    
+    <!-- Links index.css -->
+    <link rel="stylesheet" href="../css/index.css">
+    
+    <!-- Links jQuery 2.2.2 JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    
+</head>
+
+<body>
+    
+    <div id="layout">
+    
+    	
+        <!-- Menu Toggle -->
+        <a href="#menu" id="menuLink" class="menu-link">
+            
+            <!-- Menu Icon -->
+            <span></span>
+            
+        </a>
+    
+        <div id="menu">
+        
+            <div class="pure-menu">
+            
+                <a class="pure-menu-heading" href="#">WCN</a>
+    
+                <ul class="pure-menu-list">
+                
+                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
+                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+    
+                    <li class="pure-menu-item" class="menu-item-divided pure-menu-selected">
+                        <a href="#" class="pure-menu-link">Services</a>
+                    </li>
+    
+                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+                    
+                </ul>
+                
+            </div>
+            
+        </div>
+         
+        <!-- Links index.js -->
+		<script src="../js/index.js"></script>
+    
+    </div>
+    
+</body>
+
+</html>
